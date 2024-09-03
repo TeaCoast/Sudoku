085025b68aacfa9fcb3f676e4069e1a4340434b1
TeaCoast <82340176+TeaCoast@users.noreply.github.com>
2024-09-02 22:59:53 -0700

    Created SudokuMatrix in main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from dataclasses import dataclass
 
+symbols = list("123456789")
+
+class SudokuMatrix:
+    values: list[int]
+
+    def get_row(self, row_i: int):
+        return [self.values[row_i*9 + column_i] for column_i in range(9)]
+    
+    def get_column(self, column_i: int):
+        return [self.values[row_i*9 + column_i] for row_i in range(9)]
