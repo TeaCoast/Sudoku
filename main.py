e26bb7d5ac592033a1e206e9673a71f6ec3863cd
TeaCoast <teacoastone@gmail.com>
2024-09-04 02:05:15 -0700

    Added Sudoku Note String

--- main.py
+++ main.py
@@ -22,6 +22,11 @@ class SudokuMatrix:
         return [self.get_value_from_block(row_block_i, row_i, column_block_i, column_i) 
                 for column_i in range(3) for row_i in range(3)]
     
+    def num_to_str(self, num: int):
+        if num == 0:
+            return ' '
+        return str(num)
+
     def __str__(self) -> str:
         lines = []
         for row_block_i in range(3):
@@ -30,38 +35,75 @@ class SudokuMatrix:
                 for column_block_i in range(3):
                     row.append('')
                     for column_i in range(3):
-                        row[-1] += ' ' + str(self.get_value_from_block(row_block_i, row_i, column_block_i, column_i)) + ' '
+                        row[-1] += ' ' + self.num_to_str(self.get_value_from_block(row_block_i, row_i, column_block_i, column_i)) + ' '
                 lines.append('|'.join(row))
             if row_block_i < 2:
                 lines.append("-" * len(lines[-1]))
         return '\n'.join(lines)
     
-    def are_requirements_met(self) -> bool:
-        def add_requirement(requirement: bool):
-            if requirement == False:
-                raise ValueError
-        try:
-            for row_i in range(9):
-                row = self.get_row(row_i)
-                for i in range(1, 10):
-                    add_requirement(row.count(i) == 1)
-            for column_i in range(9):
-                column = self.get_column(column_i)
-                for i in range(1, 10):
-                    add_requirement(column.count(i) == 1)
-            for row_block_i in range(3):
-                for column_block_i in range(3):
-                    block = self.get_block(row_block_i, column_block_i)
-                    for i in range(1, 10):
-                        add_requirement(block.count(i) == 1)
-        except ValueError:
-            return False
-        return True
+@dataclass
+class SudokuNoteMatrix:
+    domains: list[set[int]]
 
-
-
+    def get_value(self, row_i: int, column_i: int):
+        return self.domains[row_i * 9 + column_i]
     
+    def get_value_from_block(self, row_block_i: int, row_i: int, column_block_i: int, column_i: int):
+        return self.get_value(row_block_i*3 + row_i, column_block_i*3 + column_i)
+    
+    def get_note_from_block(self, row_block_i: int, row_i: int, row_note_i: int, column_block_i: int, column_i: int, column_note_i: int):
+        return list(self.get_value(row_block_i*3 + row_i, column_block_i*3 + column_i))[row_note_i*3 + column_note_i]
 
-sudoku_matrix = SudokuMatrix([(row_i + column_i) % 9 + 1 for column_i in range(9) for row_i in range(9)])
+    def get_row(self, row_i: int) -> list[int]:
+        return [self.get_value(row_i, column_i) for column_i in range(9)]
+    
+    def get_column(self, column_i: int) -> list[int]:
+        return [self.get_value(row_i, column_i) for row_i in range(9)]
+    
+    def get_block(self, row_block_i: int, column_block_i: int) -> list[int]:
+        return [self.get_value_from_block(row_block_i, row_i, column_block_i, column_i) 
+                for column_i in range(3) for row_i in range(3)]
+    
+    def num_to_str(self, num: int):
+        if num == 0:
+            return ' '
+        return str(num)
 
-print(sudoku_matrix.are_requirements_met())
+    def __str__(self) -> str:
+        lines = []
+        # columns
+        for row_block_i in range(3):
+            lines.append([])
+            for row_i in range(3):
+                lines[row_block_i].append([])
+                for row_note_i in range(3):
+                    # columns
+                    lines[row_block_i][row_i].append([])
+                    for column_block_i in range(3):
+                        lines[row_block_i][row_i][row_note_i].append([])
+                        for column_i in range(3):
+                            lines[row_block_i][row_i][row_note_i][column_block_i].append([])
+                            for column_note_i in range(3):
+                                lines[row_block_i][row_i][row_note_i][column_block_i][column_i].append([])
+                                lines[row_block_i][row_i][row_note_i][column_block_i][column_i][column_note_i] = self.num_to_str(self.get_note_from_block(
+                                      row_block_i, row_i, row_note_i, column_block_i, column_i, column_note_i
+                                ))
+        length = len("7  8  9 | 7  8  9 | 7  8  9 || 7  8  9 | 7  8  9 | 7  8  9 || 7  8  9 | 7  8  9 | 7  8  9")
+        return ('\n' + '='*length + '\n').join(
+                    ('\n' + '-'*length + '\n').join(
+                        ('\n').join(
+                            (' || ').join(
+                                (" | ").join(
+                                    ("  ").join(
+                                        ("").join(column_note for column_note in column) 
+                                    ) for column in column_block
+                                ) for column_block in row_note
+                            ) for row_note in row
+                        ) for row in row_block
+                    ) for row_block in lines
+                )
+
+
+sudoku_matrix = SudokuNoteMatrix([{1, 2, 3, 4, 5, 6, 7, 8, 9}] * 81)
+
+print(sudoku_matrix)
